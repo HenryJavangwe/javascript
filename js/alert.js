417b1020caaf9b08be74863d9d3765059d4f2a94
Henry Javangwe <henrysparks1@gmail.com>
2020-03-09 13:30:25 +0200

    created a second javascript doc called alert and wrote a program that asks a user for the weather and then it givez them options/ ideas on what they can wear otherwise it'll suggest they pick a valid weather condition

---
+++ js/alert.js
@@ -0,0 +1,29 @@
+'use strict'
+// write a program that will tells your user what to wear when they say what the weather is like today.
+// using the if else statements and the switch and break statements to write such knd of a program.
+
+let weatherCondition = prompt('What\'s the weather today?');
+//   var sunny, windy, rainy, snowy
+if (weatherCondition == 'sunny'){
+    console.log(' ideal for short sleeved or your trendy summer wear : a pair of shorts, vests, short skirt or dress, sandals or light sneakers');
+}
+ else if ( weatherCondition == 'windy' ){
+    console.log ( ' mix up with one of these: a pair of jeans, a long sleeved shirt or a light sweater or windbreaker, a pair of sneakers or any low comfortable shoes');
+}
+  
+  else if ( weatherCondition == 'rainy' ){
+    console.log('mix up with one of these: a warm sweater, hoodie or jacket, a pair of sweat pants or jeans, a knee length dress or skirt and MOST Importantly, grab a rain Coat or an Umbrella');
+  }
+  else if (weatherCondition == 'snowy') {
+      console.log( ' it\'s a bit cold, mix up up with one of these: a warm sweater,hoodie or jacket, a pair of sweat pants or jeans, a knee length dress or skirt and MOST Importantly, grab a rain Coat or an Umbrella');
+  }
+
+  else {
+    console.log( ' It\'s not a weather condition please select from: Sunny, Windy, Snowy or Rainy');
+  }
+
+// let weather = prompt('what is the weather today?');
+
+// if (weather === 'sunny'){
+//     console.log('wear shorts!');
+// }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 'use strict';
 
-let message = 'Good morning Sunshine!';
+// let message = ' Welcome ';
 
-console.log (message);
+
+// let name = prompt ( "enter firstname");
+
+// let message2 = ( ' you are ');
+
+// let age = prompt ( ' How old are you ?');
+
+// let message3 = ' years old';
+
+// console.log (message +' '+ name +' '+ message2 + ' ' +age +' '+ message3 );
+
+// alert( name +' '+message);
+
+
+// write a program that will tells your user what to wear when they say what the weather is like today.
+// using the if else statements and the switch and break statements to write such knd of a program.
+
+// let number = Number(prompt (' enter a number'));
+
+// if ( number >0) {number + ' '+ 'is negative ';
+// } 
+// else if ( number <0 ){
+//   console.log (number + ' '+ 'is negative ');
+// }
+// else {
+//     console.log ( number + ' '+ 'is zero');
+// }
